ef91c0e3677ba1ee2ce93447a702cc4b6ecc3a8a
isikharun <isikharun@outlook.com>
2023-03-06 22:27:28 +0300

    Day06

--- src/day05_Week01_Review/Comment.java
+++ src/day05_Week01_Review/Comment.java
@@ -12,6 +12,18 @@ public class Comment {
         System.out.print("Merhaba");
         System.out.print("Merhaba");  */
 
+        String s="Harun";
+        int a,a1;
+        a=5;
+        a1=10;
+        char c='A';
+        byte b=5;
+        short s1=10;
+        long l=1000000000000l;
+        float f=2.5f;
+        double d=2.5;
+        boolean b1=true; //false
+
 
 
 
